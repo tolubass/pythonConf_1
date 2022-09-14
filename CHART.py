8fb8c5936668b4198194b63db00a17d6bdf2eb04
Tolulope Olowolaju <tolajolowolaju81@gmail.com>
2022-09-14 01:21:53 -0700

    Delete CHART.py

--- CHART.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as py
-import numpy as np
-arr = np.array([10,15,20,60,70,90,130,135,137,240])
-label = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct"]
-py.pie(arr, labels=label)
-py.show()
-
