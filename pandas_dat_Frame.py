d469ab25154808e5641b395c71c05e82c2f60b64
Tolulope Olowolaju <tolajolowolaju81@gmail.com>
2022-09-14 02:49:20 -0700

    Delete pandas_dat_Frame.py

--- pandas_dat_Frame.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import numpy as np
-exam_data = [{'name': 'Anastasia', 'score': 12.5}, {'name': 'Dima', 'score':9}, {'name': 'katherine', 'score': 16.5}]
-df = pd.DataFrame(exam_data)
-for index, row in df.iterrows():
-    print(row['name'], row['score'])
