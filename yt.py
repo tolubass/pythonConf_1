61f06764c9a905e26104708f6075f666b99bb8b5
Tolulope Olowolaju <tolajolowolaju81@gmail.com>
2022-09-14 02:52:41 -0700

    Delete yt.py

--- yt.py
+++
@@ -1,8 +0,0 @@
-def print_details(name, age, school, level, hostel, department):
-    print(f"Hello {name}! How are you?")
-    print(f"You are now {age}.")
-    print(f"i attend {school}")
-    print(f"i am in {level}")
-    print(f"and i am in {department}")
-
-print_details("Bob",20, "OAU, ILE-IFE", 200, "level", "Microbiology")
