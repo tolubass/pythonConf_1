99e6941d4a655fb514890adcad879641e5eac59d
Tolulope Olowolaju <tolajolowolaju81@gmail.com>
2022-09-14 02:35:01 -0700

    Delete def2.py

--- def2.py
+++
@@ -1,5 +0,0 @@
-from DEF import Secondary
-b = Secondary("Obafemi Awolowo University", "department of microbiology", "mcb/2016/426", "mcb201", "mcb301", "bassist ", "ball", " CACSAOAU ", "Snokers and football")
-print(b.tbass())
-
-
